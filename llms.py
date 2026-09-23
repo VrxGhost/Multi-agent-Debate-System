ef6151b115c92d0e0e2f7e3d53549deeb44c87dd
Arya Dutta <aryadutta1101@gmil.com>
2026-09-23 11:56:12 +0530

    changing models to current working one

--- llms.py
+++ llms.py
@@ -5,16 +5,16 @@ import os
 load_dotenv()
 
 pro_llm = ChatGroq(
-    model="llama-3.1-8b-instant",
+    model="openai/gpt-oss-20b",
     api_key=os.getenv("GROQ_API_KEY")
 )
 
 con_llm = ChatGroq(
-    model="meta-llama/llama-4-scout-17b-16e-instruct",
+    model="qwen/qwen3.6-27b",
     api_key=os.getenv("GROQ_API_KEY")
 )
 
 judge_llm = ChatGroq(
-    model="llama-3.3-70b-versatile",
+    model="openai/gpt-oss-120b",
     api_key=os.getenv("GROQ_API_KEY")
 )
